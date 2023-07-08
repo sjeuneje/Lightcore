66cd03c74cd9def5fd487eaa8e6a4b2f71d87632
Sacha Jeunejean <sacha.jeunejean@gmail.com>
2023-07-09 00:52:15 +0200

    Router initialized

---
+++ framework/Http/Router.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Lightcore\Framework\Http;
+
+class Router
+{
+    public $routes = [];
+
+    public function get()
+    {
+        
+    }
+
+    public function post()
+    {
+        
+    }
+
+    public function delete()
+    {
+        
+    }
+
+    public function put()
+    {
+        
+    }
+
+    public function patch()
+    {
+        
+    }
+}
