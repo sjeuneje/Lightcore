f1c715d310ff1413fe037cb32d71f64584561f8d
Sacha Jeunejean <sacha.jeunejean@gmail.com>
2023-07-09 02:37:54 +0200

    Remove Tests Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,5 @@
 <?php
 
-use App\Controllers\HomeController;
-use App\Controllers\PostController;
-
 return [
-    ['GET', '/Lightcore/public/', [HomeController::class, 'index']],
-    ['GET', '/Lightcore/public/posts/{id:\d+}', [PostController::class, 'show']]
+    //
 ];
