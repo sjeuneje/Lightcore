fd95e2fb5faa552c0fa971229cefe644ff608cc3
Sacha Jeunejean <sachajeunejean@MacBook-Pro-de-Sacha.local>
2024-02-22 00:29:59 +0100

    format welcome.php file

--- resources/views/welcome.php
+++ resources/views/welcome.php
@@ -1,12 +1,6 @@
-<?php
-/** @var $tasks */
-?>
-
-
-    <h1>A New Lightcore Project</h1>
-    <ul>
-        <?php foreach ($tasks as $task): ?>
-            <h2><?php echo $task->content; ?></h2>
-        <?php endforeach; ?>
-    </ul>
-
+<h1>A New Lightcore Project</h1>
+<ul>
+    <?php foreach ($tasks as $task): ?>
+        <h2><?php echo $task->content; ?></h2>
+    <?php endforeach; ?>
+</ul>
